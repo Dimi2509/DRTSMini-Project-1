b5caeab68344eb4c8a63b944c4c2041310df843a
yadavsushant-11 <ghm213@alumni.ku.dk>
2026-03-03 20:36:28 +0100

    Make queue update dynamic

--- Simulate-EDF.py
+++ Simulate-EDF.py
@@ -16,8 +16,8 @@ def create_task_list(task_templates: list, num_tasks):
     tasks = []
     for i in range(num_tasks):
         for template in task_templates:
-            execution_time = ceil(
-                get_execution_time(template.best_case_time, template.worst_case_time)
+            execution_time = get_execution_time(
+                template.best_case_time, template.worst_case_time
             )
             arrival_time = i * template.time_period
             tasks.append(
@@ -49,17 +49,13 @@ class Task:
         self.deadline = deadline
         self.time_period = time_period
 
+    # For priority queue to do tie-breaking based on task ID when deadlines are the same
     def __lt__(self, other):
-        return self.deadline < other.deadline
-    
-    def __gt__(self, other):
-        return self.deadline > other.deadline
-
-    def __eq__(self, value):
-        return self.deadline == value.deadline
+        return self.id < other.id
 
     def __str__(self):
         return f"Task(id={self.id}, arrival_time={self.arrival_time}, remaining_time={self.remaining_time}, deadline={self.deadline}, time_period={self.time_period})"
+
 
 class InternalJob:
     def __init__(self, id, deadline, start_time, end_time, time_period, execution_time):
@@ -74,67 +70,116 @@ class InternalJob:
     def __str__(self):
         return f"Job(name={self.name}, start_time={self.start_time}, end_time={self.end_time}, deadline={self.deadline}, time_period={self.time_period}, execution_time={self.execution_time})"
 
+class SchedulingQueue:
+    def __init__(self):
+        self.queue = queue.PriorityQueue()
+
+    def put(self, task):
+        self.queue.put((task.arrival_time, task))
+
+    def pop(self):
+        return self.queue.get()[1]
+    
+    def peek(self):
+        if not self.queue.empty():
+            return self.queue.queue[0][1]
+        return None
+
+    def empty(self):
+        return self.queue.empty()
+    
+    def print(self):
+        self.queue.queue.sort()  # Sort the internal list to ensure correct order for printing
+        for item in self.queue.queue:
+            print(item[1])
+
+class ReadyQueue:
+    def __init__(self):
+        self.queue = queue.PriorityQueue()
+
+    def put(self, task):
+        self.queue.put((task.deadline, task))
+
+    def pop(self):
+        return self.queue.get()[1]
+    
+    def peek(self):
+        if not self.queue.empty():
+            return self.queue.queue[0][1]
+        return None
+
+    def empty(self):
+        return self.queue.empty()
+
 
 class EDFScheduler:
     def __init__(self, task_templates=None):
-        self.ready_queue = queue.PriorityQueue()
+        self.ready_queue = ReadyQueue()
+        self.scheduling_queue = SchedulingQueue()
         self.task_templates = task_templates if task_templates is not None else []
         self.current_time = 0
         self.job_log = []
         self.current_job: InternalJob = None
 
-    def add_task(self, task):
-        self.ready_queue.put(task)
-
     def run(self):
-        self.ready_queue.queue.sort()  # Ensure the ready queue is sorted by deadline
-        # for task in self.ready_queue.queue:
-        #     print(task)
-        while not self.ready_queue.empty():
-            top_ready_task = (
-                self.ready_queue.queue[0] if not self.ready_queue.empty() else None
-            )
+        self.scheduling_queue.print()  # Print the scheduling queue before starting the simulation
+
+        while not (self.ready_queue.empty() and self.scheduling_queue.empty()):
+
+            self.update_ready_queue()
+            top_ready_task = self.ready_queue.peek()
+
             # If processor is idle and there is a ready task, start executing the top ready task
             if (
                 self.current_job is None
                 and top_ready_task is not None
-                and top_ready_task.arrival_time <= self.current_time
             ):
-                self.current_job = self.get_internal_job_from_task(top_ready_task)
-                self.ready_queue.queue.pop(0)
+                self.current_job = self.get_internal_job_from_task(
+                    self.ready_queue.pop()
+                )
+
             # Context change condition: if the top ready task has an earlier deadline than the second ready job,
             # preempt the current job
             elif (
                 top_ready_task is not None
-                and top_ready_task.arrival_time <= self.current_time
-                and (self.current_job is None or top_ready_task.deadline < self.current_job.deadline)
+                and (
+                    self.current_job is None
+                    or top_ready_task.deadline < self.current_job.deadline
+                )
             ):
-                if self.current_job is not None:
-                    self.current_job.end_time = self.current_time
-                    self.job_log.append(self.current_job)
-                    task_to_requeue = self.get_task_from_internal_job(self.current_job)
-                    self.ready_queue.put(task_to_requeue)
-                self.current_job = self.get_internal_job_from_task(top_ready_task)
-                self.ready_queue.queue.pop(0)
+                self.log_job(self.current_job)
+                self.ready_queue.put(
+                    self.get_task_from_internal_job(self.current_job)
+                )
+                self.current_job = self.get_internal_job_from_task(
+                    self.ready_queue.pop()
+                )
 
             # If there is a current job, execute it for one time unit
             if self.current_job is not None:
-                self.current_job.execution_time -= 1 
-                if self.current_job.execution_time <= 0:
-                    self.current_job.end_time = self.current_time + 1
-                    self.log_job(self.current_job)
-                    self.current_job = None
+                self.current_job.execution_time -= 1
+                self.log_job_if_finished(self.current_job)
             self.current_time += 1
-    
+
     def log_job(self, job: InternalJob):
-        self.job_log.append(Job(
-            id=job.id,
-            deadline=job.deadline,
-            start_time=job.start_time,
-            end_time=job.end_time,
-            time_period=job.time_period
-        ))
-    
+        self.job_log.append(
+            Job(
+                id=job.id,
+                deadline=job.deadline,
+                start_time=job.start_time,
+                end_time=(
+                    job.end_time if job.end_time is not None else self.current_time
+                ),
+                time_period=job.time_period,
+            )
+        )
+
+    def log_job_if_finished(self, job: InternalJob):
+        if job.execution_time <= 0:
+            job.end_time = self.current_time + 1
+            self.log_job(job)
+            self.current_job = None
+
     def get_internal_job_from_task(self, task: Task):
         return InternalJob(
             id=task.id,
@@ -142,17 +187,24 @@ class EDFScheduler:
             start_time=self.current_time,
             end_time=None,
             time_period=task.time_period,
-            execution_time=task.remaining_time
+            execution_time=task.remaining_time,
         )
-    
+
     def get_task_from_internal_job(self, job: InternalJob):
         return Task(
             id=job.id,
             arrival_time=self.current_time,
             execution_time=job.execution_time,
             deadline=job.deadline,
-            time_period=job.time_period
+            time_period=job.time_period,
         )
+
+    def update_ready_queue(self):
+        while (
+            not self.scheduling_queue.empty()
+            and self.scheduling_queue.peek().arrival_time <= self.current_time
+        ):
+            self.ready_queue.put(self.scheduling_queue.pop())
 
 
 class EDFSimulation:
@@ -162,13 +214,14 @@ class EDFSimulation:
 
     def run(self):
         for task in self.ready_tasks:
-            self.scheduler.add_task(task)
+            self.scheduler.scheduling_queue.put(task)
         self.scheduler.run()
         return self.scheduler.job_log
 
 
-task1 = TaskTemplate(1, 2, 10, 12, 12)
-task2 = TaskTemplate(2, 1, 5, 5, 5)
-simulation = EDFSimulation([task1, task2], 10).run()
+task1 = TaskTemplate(1, 50, 60, 100, 80)
+task2 = TaskTemplate(2, 22, 25, 25, 20)
+task3 = TaskTemplate(3, 6, 8, 16, 14)
+simulation = EDFSimulation([task1, task3], 10).run()
 for job in simulation:
     print(job)
