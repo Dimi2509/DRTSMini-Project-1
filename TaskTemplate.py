f240d0ac026e2fd03ec3ecc786a6d52603975211
yadavsushant-11 <ghm213@alumni.ku.dk>
2026-03-06 09:18:28 +0100

    Add PE field in tasktemplate

--- TaskTemplate.py
+++ TaskTemplate.py
@@ -1,8 +1,9 @@
 class TaskTemplate:
-    def __init__(self, id, best_case_time, worst_case_time, time_period, deadline, jitter):
+    def __init__(self, id, best_case_time, worst_case_time, time_period, deadline, jitter, pe):
         self.id = id
         self.best_case_time = best_case_time
         self.worst_case_time = worst_case_time
         self.time_period = time_period
         self.deadline = deadline
         self.jitter = jitter if jitter is not None else 0
+        self.pe = pe if pe is not None else 0
